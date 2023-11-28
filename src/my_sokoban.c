796935aaeec27a6be7032c7a9a28b84f364be587
Yanis <yanis.kernoua@epitech.eu>
2023-11-28 19:25:10 +0100

    feat(src): Game structure

--- src/my_sokoban.c
+++ src/my_sokoban.c
@@ -13,11 +13,19 @@
 #include <unistd.h>
 #include "include/my_strings.h"
 #include "include/my_std.h"
+#include "player.h"
+
+void display_map(char **map)
+{
+    for (int i = 0; map[i] != NULL; i++)
+        mvprintw(i, 0, "%s", map[i]);
+}
 
 int main(int ac, char **av)
 {
     char *buffer = NULL;
     char **map = NULL;
+    player_t *player = NULL;
 
     buffer = file_to_buffer(av[1]);
     if (buffer == NULL)
@@ -25,8 +33,10 @@ int main(int ac, char **av)
     map = buffer_to_array(buffer, '\n');
     if (map == NULL)
         return 84;
-    for (int i = 0; map[i] != NULL; i++)
-        printf("%s\n", map[i]);
+    player = create_player(map);
+    if (player == NULL)
+        return 84;
+    printf("Player position: %d, %d\n", player->pos->x, player->pos->y);
     free(buffer);
     my_freearray(map);
     return (0);
--- src/my_sokoban.h
+++ src/my_sokoban.h
@@ -7,7 +7,22 @@
 
 #pragma once
 
+#include "box.h"
+#include "storage.h"
+#include "player.h"
+
 typedef enum game_state_e {
     WIN = 0,
     DEFEAT = 1,
+    PLAYING = 2
 } game_state_t;
+
+typedef struct game_s {
+    char **map;
+    player_t *player;
+    box_t **boxes;
+    storage_t **storages;
+    int nb_boxes;
+    int nb_storages;
+    game_state_t state;
+} game_t;
