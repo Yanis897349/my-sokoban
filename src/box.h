765ac34785bd7afde174e5ec1dbd8199b93d5da9
Yanis <yanis.kernoua@epitech.eu>
2023-12-04 17:59:24 +0100

    feat(src): Box logic

--- src/box.h
+++ src/box.h
@@ -7,10 +7,12 @@
 
 #pragma once
 #include "position.h"
+#include "player.h"
 
 typedef struct box_s {
     char c;
     position_t *pos;
 } box_t;
 
-box_t *create_box(char **map);
+void move_box(int key, player_t *player, char **map);
+box_t **create_boxes(char **map, int box_count);
